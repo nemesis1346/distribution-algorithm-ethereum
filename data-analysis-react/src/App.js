5bb254a6baa7b4d53721db1d3c822dfd51ac6808
nemesis1346 <marco.maigua1346@gmail.com>
2019-01-31 15:29:31 -0600

    starting to develop the flow diagram

--- data-analysis-react/src/App.js
+++ data-analysis-react/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from "d3";
+
+const TREE_OPTION = {
+  tooltip: {
+    trigger: 'item',
+    triggerOn: 'mousemove'
+  },
+  series: [
+    {
+      type: 'tree',
+      name: 'tree1',
+      data: [],
+      top: '5%',
+      left: '7%',
+      bottom: '2%',
+      right: '60%',
+
+      symbolSize: 7,
+
+      label: {
+        normal: {
+          position: 'left',
+          verticalAlign: 'middle',
+          align: 'right'
+        }
+      },
+
+      leaves: {
+        label: {
+          normal: {
+            position: 'right',
+            verticalAlign: 'middle',
+            align: 'left'
+          }
+        }
+      },
+      expandAndCollapse: false,  //This is for expanding the nodes or not
+    }
+  ]
+};
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" ame="isrc" placeholder="Track ISRC"></input>
+        <button ion-button type="submit" >Update</button>
+        <div echarts style="height: 600px;"></div>
       </div>
     );
   }
