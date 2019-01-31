53eb210819e94647676a5504746dcf325d335d45
nemesis1346 <marco.maigua1346@gmail.com>
2019-01-31 13:54:57 -0600

    examples 3 and 4 working

--- hyperstate/test/data-analysis/example3.js
+++ hyperstate/test/data-analysis/example3.js
@@ -1,57 +1,51 @@
-async function example3(){
-         //EXAMPLE 3 ///////////////////////////////////////////////////////////////////(STILL MUST BE FIXED)
+'use strict';
+const UUID = require('uuid/v1');
+const methods = require('./methods.js');
+
+//FAILING
+async function example3() {
+
+      //INIT DATA////////////////////////////////////////////////////////////// 
+      let trackId = UUID();
+      await methods.createTrack(trackId, 'track', 100, 'vendorIdentifier', 'label', 'author', 'MUSICIAN');
+
+      let trader1 = UUID();
+      await methods.createParticipant(trader1, 'trader1', 'trader1', 0.00, 'DISTRIBUTOR', trader1);
+      let trader2 = UUID();
+      await methods.createParticipant(trader2, 'trader2', 'trader2', 0.00, 'DISTRIBUTOR', trader2);
+      let trader3 = UUID();
+      await methods.createParticipant(trader3, 'trader3', 'trader3', 0.00, 'ARTIST', trader3);
+      let trader4 = UUID();
+      await methods.createParticipant(trader4, 'trader4', 'trader4', 0.00, 'ARTIST', trader4);
+
+      //EXAMPLE 3 ///////////////////////////////////////////////////////////////////(STILL MUST BE FIXED)
       //Track1
-      // let agreement1Id = UUID();
-      // await createAgreement(agreement1Id, orchardId, membranId, 0.25, 'PENDING', trackId, 'Orchard', 'Membran');
-      // let agreement2Id = UUID();
-      // await createAgreement(agreement2Id, membranId, artist1Id, 0.5, 'PENDING', trackId, 'Membran', 'Artist2');
-      // let agreement3Id = UUID();
-      // await createAgreement(agreement3Id, artist1Id, orchardId, 0.5, 'PENDING', trackId, 'Artist1', 'Orchard');
-      // let agreement4Id = UUID();
-      // await createAgreement(agreement4Id, orchardId, artist2Id, 0.25, 'PENDING', trackId, 'Orchard', 'Artist2');
-      // //Track 2
-      // let agreement5Id = UUID();
-      // await createAgreement(agreement5Id, orchardId, membranId, 0.25, 'PENDING', track2Id, 'Orchard', 'Membran');
-      // let agreement6Id = UUID();
-      // await createAgreement(agreement6Id, membranId, artist1Id, 0.5, 'PENDING', track2Id, 'Membran', 'Artist2');
-      // let agreement7Id = UUID();
-      // await createAgreement(agreement7Id, artist1Id, orchardId, 0.5, 'PENDING', track2Id, 'Artist1', 'Orchard');
-      // let agreement8Id = UUID();
-      // await createAgreement(agreement8Id, orchardId, artist2Id, 0.25, 'PENDING', track2Id, 'Orchard', 'Artist2');
-      // //Track 3
-      // let agreement9Id = UUID();
-      // await createAgreement(agreement9Id, orchardId, membranId, 0.25, 'PENDING', track3Id, 'Orchard', 'Membran');
-      // let agreement10Id = UUID();
-      // await createAgreement(agreement10Id, membranId, artist1Id, 0.5, 'PENDING', track3Id, 'Membran', 'Artist2');
-      // let agreement11Id = UUID();
-      // await createAgreement(agreement11Id, artist1Id, orchardId, 0.5, 'PENDING', track3Id, 'Artist1', 'Orchard');
-      // let agreement12Id = UUID();
-      // await createAgreement(agreement12Id, orchardId, artist2Id, 0.25, 'PENDING', track3Id, 'Orchard', 'Artist2');
-      // //Track 4
-      // let agreement13Id = UUID();
-      // await createAgreement(agreement13Id, orchardId, membranId, 0.25, 'PENDING', track4Id, 'Orchard', 'Membran');
-      // let agreement14Id = UUID();
-      // await createAgreement(agreement14Id, membranId, artist1Id, 0.5, 'PENDING', track4Id, 'Membran', 'Artist2');
-      // let agreement15Id = UUID();
-      // await createAgreement(agreement15Id, artist1Id, orchardId, 0.5, 'PENDING', track4Id, 'Artist1', 'Orchard');
-      // let agreement16Id = UUID();
-      // await createAgreement(agreement16Id, orchardId, artist2Id, 0.25, 'PENDING', track4Id, 'Orchard', 'Artist2');
+      let agreement1Id = UUID();
+      await methods.createAgreement(agreement1Id, trader1, trader2, 0.25, 'PENDING', trackId, 'trader1', 'trader2');
+      let agreement2Id = UUID();
+      await methods.createAgreement(agreement2Id, trader2, trader3, 0.5, 'PENDING', trackId, 'trader2', 'trader4');
+      let agreement3Id = UUID();
+      await methods.createAgreement(agreement3Id, trader3, trader1, 0.5, 'PENDING', trackId, 'trader3', 'trader1');
+      let agreement4Id = UUID();
+      await methods.createAgreement(agreement4Id, trader1, trader4, 0.25, 'PENDING', trackId, 'trader1', 'trader4');
 
-      // await updateTrack(trackId, 10);
-      // await distribution(trackId, membranId, String(new Date().toISOString()));
-      // await updateTrack(track2Id, 10);
-      // await distribution(track2Id, membranId, String(new Date().toISOString()));
-      // await updateTrack(track3Id, 10);
-      // await distribution(track3Id, membranId, String(new Date().toISOString()));
-      // await updateTrack(track4Id, 10);
-      // await distribution(track4Id, membranId, String(new Date().toISOString()));
+      await methods.updateTrack(trackId, 10);
+      await methods.distribution(trackId, trader2, String(new Date().toISOString()));
+      await methods.updateTrack(trackId, 10);
+      await methods.distribution(trackId, trader2, String(new Date().toISOString()));
+      await methods.updateTrack(trackId, 10);
+      await methods.distribution(trackId, trader2, String(new Date().toISOString()));
+      await methods.updateTrack(trackId, 10);
+      await methods.distribution(trackId, trader2, String(new Date().toISOString()));
 
-      // console.log('TOKEN MEMBRAN');
-      // await getTokenAccount(membranId);
-      // console.log('TOKEN ARTIST 1');
-      // await getTokenAccount(artist1Id);
-      // console.log('TOKEN ARTIST 2');
-      // await getTokenAccount(artist2Id);
-      // console.log('TOKEN ORCHARD');
-      // await getTokenAccount(orchardId);
+      console.log('TOKEN TRADER 1');
+      await methods.getTokenAccount(trader1);
+      console.log('TOKEN TRADER 2');
+      await methods.getTokenAccount(trader2);
+      console.log('TOKEN TRADER 3');
+      await methods.getTokenAccount(trader3);
+      console.log('TOKEN TRADER 4');
+      await methods.getTokenAccount(trader4);   
 }
+
+example3();
--- hyperstate/test/data-analysis/example4.js
+++ hyperstate/test/data-analysis/example4.js
@@ -1,68 +1,58 @@
-async function example4(){
+'use strict';
+const UUID = require('uuid/v1');
+const methods = require('./methods.js');
 
-      //EXAMPLE 4///////////////////////////////////////////////////////////////////////////////
-      //Track1
-    //   let agreement1Id = UUID();
-    //   await createAgreement(agreement1Id, membranId, artist1Id, 0.25, 'PENDING', trackId, 'Membran', 'Artist1');
-    //   let agreement2Id = UUID();
-    //   await createAgreement(agreement2Id, membranId, artist2Id, 0.25, 'PENDING', trackId, 'Membran', 'Artist2');
-    //   let agreement3Id = UUID();
-    //   await createAgreement(agreement3Id, artist1Id, orchardId, 0.5, 'PENDING', trackId, 'Artist1', 'Orchard');
-    //   let agreement4Id = UUID();
-    //   await createAgreement(agreement4Id, artist2Id, orchardId, 0.5, 'PENDING', trackId, 'Artist2', 'Orchard');
-    //   let agreement5Id = UUID();
-    //   await createAgreement(agreement5Id, orchardId, artist3Id, 0.5, 'PENDING', trackId, 'Orchard', 'Artist3');
-    //   //Track2
-    //   let agreement6Id = UUID();
-    //   await createAgreement(agreement6Id, membranId, artist1Id, 0.25, 'PENDING', track2Id, 'Membran', 'Artist1');
-    //   let agreement7Id = UUID();
-    //   await createAgreement(agreement7Id, membranId, artist2Id, 0.25, 'PENDING', track2Id, 'Membran', 'Artist2');
-    //   let agreement8Id = UUID();
-    //   await createAgreement(agreement8Id, artist1Id, orchardId, 0.5, 'PENDING', track2Id, 'Artist1', 'Orchard');
-    //   let agreement9Id = UUID();
-    //   await createAgreement(agreement9Id, artist2Id, orchardId, 0.5, 'PENDING', track2Id, 'Artist2', 'Orchard');
-    //   let agreement10Id = UUID();
-    //   await createAgreement(agreement10Id, orchardId, artist3Id, 0.5, 'PENDING', track2Id, 'Orchard', 'Artist3');
-    //   //Track3
-    //   let agreement11Id = UUID();
-    //   await createAgreement(agreement11Id, membranId, artist1Id, 0.25, 'PENDING', track3Id, 'Membran', 'Artist1');
-    //   let agreement12Id = UUID();
-    //   await createAgreement(agreement12Id, membranId, artist2Id, 0.25, 'PENDING', track3Id, 'Membran', 'Artist2');
-    //   let agreement13Id = UUID();
-    //   await createAgreement(agreement13Id, artist1Id, orchardId, 0.5, 'PENDING', track3Id, 'Artist1', 'Orchard');
-    //   let agreement14Id = UUID();
-    //   await createAgreement(agreement14Id, artist2Id, orchardId, 0.5, 'PENDING', track3Id, 'Artist2', 'Orchard');
-    //   let agreement15Id = UUID();
-    //   await createAgreement(agreement15Id, orchardId, artist3Id, 0.5, 'PENDING', track3Id, 'Orchard', 'Artist3');
-    //   //Track4
-    //   let agreement16Id = UUID();
-    //   await createAgreement(agreement16Id, membranId, artist1Id, 0.25, 'PENDING', track4Id, 'Membran', 'Artist1');
-    //   let agreement17Id = UUID();
-    //   await createAgreement(agreement17Id, membranId, artist2Id, 0.25, 'PENDING', track4Id, 'Membran', 'Artist2');
-    //   let agreement18Id = UUID();
-    //   await createAgreement(agreement18Id, artist1Id, orchardId, 0.5, 'PENDING', track4Id, 'Artist1', 'Orchard');
-    //   let agreement19Id = UUID();
-    //   await createAgreement(agreement19Id, artist2Id, orchardId, 0.5, 'PENDING', track4Id, 'Artist2', 'Orchard');
-    //   let agreement20Id = UUID();
-    //   await createAgreement(agreement20Id, orchardId, artist3Id, 0.5, 'PENDING', track4Id, 'Orchard', 'Artist3');
+async function example4() {
+    //INIT DATA////////////////////////////////////////////////////////////// 
+    let trackId = UUID();
+    await methods.createTrack(trackId, 'track', 100, 'vendorIdentifier', 'label', 'author', 'MUSICIAN');
+    let track2Id = UUID();
+    await methods.createTrack(track2Id, 'track2', 100, 'vendorIdentifier', 'label', 'author', 'MUSICIAN');
 
-    //   await updateTrack(trackId, 10);
-    //   await distribution(trackId, membranId, String(new Date().toISOString()));
-    //   await updateTrack(track2Id, 10);
-    //   await distribution(track2Id, membranId, String(new Date().toISOString()));
-    //   await updateTrack(track3Id, 10);
-    //   await distribution(track3Id, membranId, String(new Date().toISOString()));
-    //   await updateTrack(track4Id, 10);
-    //   await distribution(track4Id, membranId, String(new Date().toISOString()));
+    let trader1 = UUID();
+    await methods.createParticipant(trader1, 'trader1', 'trader1', 0.00, 'DISTRIBUTOR', trader1);
+    let trader2 = UUID();
+    await methods.createParticipant(trader2, 'trader2', 'trader2', 0.00, 'DISTRIBUTOR', trader2);
+    let trader3 = UUID();
+    await methods.createParticipant(trader3, 'trader3', 'trader3', 0.00, 'ARTIST', trader3);
+    let trader4 = UUID();
+    await methods.createParticipant(trader4, 'trader4', 'trader4', 0.00, 'ARTIST', trader4);
+    let trader5 = UUID();
+    await methods.createParticipant(trader5, 'trader5', 'trader5', 0.00, 'ARTIST', trader5);
 
-    //   console.log('TOKEN MEMBRAN');
-    //   await getTokenAccount(membranId);
-    //   console.log('TOKEN ARTIST 1');
-    //   await getTokenAccount(artist1Id);
-    //   console.log('TOKEN ARTIST 2');
-    //   await getTokenAccount(artist2Id);
-    //   console.log('TOKEN ARTIST 3');
-    //   await getTokenAccount(artist3Id);
-    //   console.log('TOKEN ORCHARD');
-    //   await getTokenAccount(orchardId);
+    //EXAMPLE 4///////////////////////////////////////////////////////////////////////////////
+    //Track1
+    let agreement1Id = UUID();
+    await methods.createAgreement(agreement1Id, trader1, trader2, 0.25, 'PENDING', trackId, 'trader1', 'trader2');
+    let agreement2Id = UUID();
+    await methods.createAgreement(agreement2Id, trader1, trader3, 0.25, 'PENDING', trackId, 'trader1', 'trader3');
+    let agreement3Id = UUID();
+    await methods.createAgreement(agreement3Id, trader2, trader4, 0.5, 'PENDING', trackId, 'trader2', 'trader4');
+    let agreement4Id = UUID();
+    await methods.createAgreement(agreement4Id, trader3, trader4, 0.5, 'PENDING', trackId, 'trader3', 'trader4');
+    let agreement5Id = UUID();
+    await methods.createAgreement(agreement5Id, trader4, trader5, 0.5, 'PENDING', trackId, 'trader4', 'trader5');
+
+    await methods.updateTrack(trackId, 10);
+    await methods.distribution(trackId, trader1, String(new Date().toISOString()));
+    await methods.updateTrack(trackId, 10);
+    await methods.distribution(trackId, trader1, String(new Date().toISOString()));
+    await methods.updateTrack(trackId, 10);
+    await methods.distribution(trackId, trader1, String(new Date().toISOString()));
+    await methods.updateTrack(trackId, 10);
+    await methods.distribution(trackId, trader1, String(new Date().toISOString()));
+
+    console.log('TOKEN TRADER 1');
+    await methods.getTokenAccount(trader1);
+    console.log('TOKEN TRADER2');
+    await methods.getTokenAccount(trader2);
+    console.log('TOKEN TRADER 3');
+    await methods.getTokenAccount(trader3);
+    console.log('TOKEN TRADER 4');
+    await methods.getTokenAccount(trader4);
+    console.log('TOKEN TRADER 5');
+    await methods.getTokenAccount(trader5);
+
 }
+
+example4();
