157c78d7284e6b502d3ba2b36ecb4d2a252edab8
nemesis1346 <marco.maigua1346@gmail.com>
2019-02-04 19:04:49 -0600

    starting to work with different threads

--- hyperstate/test/data-analysis/performanceExamples/testingProcess.js
+++ hyperstate/test/data-analysis/performanceExamples/testingProcess.js
@@ -1,14 +1,34 @@
-const cp  = require('child_process');
-
+const cp = require('child_process');
+const { promisifyChildProcess } = require('promisify-child-process');
+const Example1 = require('./example1-old-form.js');
 
 // cp.fork('./example1-performance.js',['example1']);
 // cp.fork('./example1-performance.js',['example1']);
 // cp.fork('./example1-performance.js',['example1']);
 // cp.fork('./example1-performance.js',['example1']);
-const fork = cp.fork('./example1-performance.js');
-fork.send('this is an input');
+// const fork = cp.fork('./example1-performance.js');
+// fork.send('this is an input');
+// fork.send('this is an input');
+// fork.send('this is an input');
+// fork.send('this is an input');
 
-fork.on('message',function(result){
-    console.log(result);
-});
+// fork.on('message',function(result){
+//     console.log(result);
+// });
 
+
+async function main() {
+    try {
+        this.example1 = new Example1();
+        console.log(this.example1);
+        promisifyChildProcess(this.example1.executeExample1(), 'test');
+        promisifyChildProcess(this.example1.executeExample1(), 'test');
+        promisifyChildProcess(this.example1.executeExample1(), 'test');
+        promisifyChildProcess(this.example1.executeExample1(), 'test');
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+main();
