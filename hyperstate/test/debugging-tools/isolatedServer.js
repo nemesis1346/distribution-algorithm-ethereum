f5693a8b6665cdc031e703c7b53b641ed40768ca
nemesis1346 <marco.maigua1346@gmail.com>
2019-02-05 18:18:35 -0600

    kind of is working

--- hyperstate/test/debugging-tools/isolatedServer.js
+++ hyperstate/test/debugging-tools/isolatedServer.js
@@ -5,164 +5,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const IsolatedChaincode = require("./isolatedChaincode.js");
-const DataModel = require("../../models/dataModel.js");
 const cp = require("child_process");
+const { exec, spawn, fork, execFile } = require('promisify-child-process')
+const { promisifyChildProcess } = require('promisify-child-process');
 
 const app = express();
 
 const handler = async (request, response) => {
-  const { headers, method, url } = request;
-  let buffer = [];
-  request
-    .on("error", err => {
-      console.log("Error", err);
-    })
-    .on("data", chunk => {
-      buffer.push(chunk);
-    })
-    .on("end", async () => {
-      let bufferContent = Buffer.concat(buffer).toString();
-
-      //Set response
-      response.statusCode = 200;
-      response.setHeader("Content-Type", "application/json");
-      response.setHeader("Access-Control-Allow-Origin", "*");
-      response.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,PUT,POST,DELETE,OPTIONS"
-      );
-      response.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, Content-Length, X-Requested-With"
-      );
-      response.on("error", err => {
-        console.error(err);
-      });
-      //Call method
-      let promise;
-      let dataModel = new DataModel(null, null, null);
-
-      try {
-        switch (url) {
-          case "/createTrack":
-            promise = this.isolatedChaincode.createTrack(
-              JSON.parse(bufferContent)
+    const { headers, method, url } = request;
+    let buffer = [];
+    request
+        .on("error", err => {
+            console.log("Error", err);
+        })
+        .on("data", chunk => {
+            buffer.push(chunk);
+        })
+        .on("end", async () => {
+            let bufferContent = Buffer.concat(buffer).toString();
+            console.log(bufferContent);
+            //Set response
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "application/json");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET,PUT,POST,DELETE,OPTIONS"
             );
-            break;
-          default:
-            dataModel.message = "Method not found";
-            dataModel.status = "405";
-            let body = JSON.stringify(dataModel);
-
-            console.log("STATUS 405: ");
-            console.log("Method not found");
-            const responseBody = { headers, method, url, body };
-
-            response.statusCode = 405;
-            response.write(JSON.stringify(responseBody));
-            response.end();
-            break;
-        }
-
-        //Executing the promise , maybe need POST and GET
-        if (promise != null) {
-        //   const fork = cp.fork("./promiseProcessHandler.js");
-        //   fork.send(promise);
-        //   fork.on("message", function(result) {
-        //     console.log(result);
-        //     //This is status 200 , everything ok
-        //     if (result) {
-        //       if (result.status == "200") {
-        //         dataModel.data = result;
-        //         dataModel.status = "200";
-        //       } else {
-        //         console.log(dataModel);
-        //         dataModel.message = result;
-        //         dataModel.status = "300";
-        //       }
-        //     } else {
-        //       console.log("Something went wrong");
-        //       console.log(result);
-        //     }
-        //     let body = JSON.stringify(dataModel);
-        //     console.log("STATUS 200: ");
-        //     console.log(body);
-        //     const responseBody = { headers, method, url, body };
-
-        //     response.statusCode = 200;
-        //     response.write(JSON.stringify(responseBody));
-        //     response.end();
-        //   });
-
-        //   fork.on("error", function(error) {
-        //     console.log("ERROR IN GATE");
-
-        //     dataModel.message = error.message.toString();
-        //     dataModel.status = "400";
-        //     let body = JSON.stringify(dataModel);
-        //     console.log("ERROR 400:");
-        //     console.log(dataModel);
-        //     const responseBody = { headers, method, url, body };
-
-        //     response.statusCode = 300;
-        //     response.write(JSON.stringify(responseBody));
-        //     response.end();
-        //   });
-            promise
-              .then(function(result) {
-                console.log(result);
-                //This is status 200 , everything ok
-                if (result) {
-                  if (result.status == "200") {
-                    dataModel.data = result;
-                    dataModel.status = "200";
-                  } else {
-                    console.log(dataModel);
-                    dataModel.message = result;
-                    dataModel.status = "300";
-                  }
-                } else {
-                  console.log("Something went wrong");
-                  console.log(result);
-                }
-                let body = JSON.stringify(dataModel);
-                console.log("STATUS 200: ");
-                console.log(body);
-                const responseBody = { headers, method, url, body };
-
-                response.statusCode = 200;
-                response.write(JSON.stringify(responseBody));
-                response.end();
-              })
-              .catch(error => {
-                console.log("ERROR IN GATE");
-
-                dataModel.message = error.message.toString();
-                dataModel.status = "400";
-                let body = JSON.stringify(dataModel);
-                console.log("ERROR 400:");
-                console.log(dataModel);
-                const responseBody = { headers, method, url, body };
-
-                response.statusCode = 300;
-                response.write(JSON.stringify(responseBody));
-                response.end();
-              });
-        }
-      } catch (error) {
-        dataModel.message = error.message.toString();
-        dataModel.status = "500";
-        let body = JSON.stringify(dataModel);
-        console.log("ERROR 500:");
-        console.log(error);
-        const responseBody = { headers, method, url, body };
-
-        response.statusCode = 500;
-        response.write(JSON.stringify(responseBody));
-        response.end();
-      }
-    });
+            response.setHeader(
+                "Access-Control-Allow-Headers",
+                "Content-Type, Authorization, Content-Length, X-Requested-With"
+            );
+            response.on("error", err => {
+                console.error(err);
+            });
+            await fork('./promiseProcessHandler.js', ['message'], { encoding: 'utf8' })
+                .on("message", (result) => {
+                    console.log(result);
+                    response.statusCode = 405;
+                    response.write(result);
+                    response.end();
+                })
+                .send(JSON.parse(bufferContent));
+        });
 };
 
 app.post("/createTrack", handler);
@@ -172,13 +57,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(port, async err => {
-  if (err) {
-    return console.log("something bad happened", err);
-  }
-  try {
-    this.isolatedChaincode = new IsolatedChaincode();
-  } catch (error) {
-    console.log("Error Composer instance: ", error);
-  }
-  console.log("server is listening on: ", port);
+    if (err) {
+        return console.log("something bad happened", err);
+    }
+    console.log("server is listening on: ", port);
 });
