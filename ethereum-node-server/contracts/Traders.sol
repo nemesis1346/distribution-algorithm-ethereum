3cf7840c5af10c6d4a8e3c9cfab3cf4af6a077bf
root <marco.maigua1346@gmail.com>
2019-03-17 16:08:10 -0600

    second version

--- ethereum-node-server/contracts/Traders.sol
+++ ethereum-node-server/contracts/Traders.sol
@@ -26,3 +26,36 @@ contract Traders{
 
     event LogTraderCreation(string);
 }    
+
+
+//Version 2
+pragma solidity ^0.5.0;
+
+//This is the best way to structure a digital entity in a contract
+contract Tracks{
+    
+    struct Track{
+        uint id;
+        string isrc;
+        string title;
+        uint revenueTotal;
+        address uploaderEOA;
+    }
+
+    mapping(uint=>Track) public tracks;
+
+
+    function createTrack(uint id, string memory isrc, string memory title, uint revenueTotal, string memory uploaderId) public{
+        address uploaderEOA = msg.sender; 
+        tracks[id]=Track(id, isrc, title, revenueTotal, uploaderEOA);
+        emit LogTrack("Track created!!!"); //this is an event 
+
+    }
+    
+    function getTrack(uint id) public returns(uint, string memory,string memory, uint, address) {
+        return (tracks[id].id, tracks[id].isrc,tracks[id].title,tracks[id].revenueTotal,tracks[id].uploaderEOA);
+    }
+
+    event LogTrack(string);
+
+}    
