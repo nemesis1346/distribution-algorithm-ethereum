d2142e852b0da0c5a991dafedad8811024d47ea4
tony <nemesis1346@uofs-10-230-64-199.usask.ca>
2019-05-09 16:18:48 -0600

    getting connections

--- ethereum-node-server/test/data-analysis/accuracyExamples/example1ServerConnection.js
+++ ethereum-node-server/test/data-analysis/accuracyExamples/example1ServerConnection.js
@@ -31,158 +31,158 @@ async function example1() {
             gasLimit);
 
         //Creating Traders
-        await methods.createTrader(
-            trader1,
-            "trader1",
-            trader1,
-            await methods.getTAContractAddress(),
-            trader1,
-            gasLimit);
+        // await methods.createTrader(
+        //     trader1,
+        //     "trader1",
+        //     trader1,
+        //     await methods.getTAContractAddress(),
+        //     trader1,
+        //     gasLimit);
 
-        await methods.createTrader(
-            trader2,
-            "trader2",
-            trader2,
-            await methods.getTAContractAddress(),
-            trader2,
-            gasLimit);
+        // await methods.createTrader(
+        //     trader2,
+        //     "trader2",
+        //     trader2,
+        //     await methods.getTAContractAddress(),
+        //     trader2,
+        //     gasLimit);
 
-        await methods.createTrader(
-            trader3,
-            "trader3",
-            trader3,
-            await methods.getTAContractAddress(),
-            trader3,
-            gasLimit);
+        // await methods.createTrader(
+        //     trader3,
+        //     "trader3",
+        //     trader3,
+        //     await methods.getTAContractAddress(),
+        //     trader3,
+        //     gasLimit);
 
 
-        await methods.createTrader(
-            trader4,
-            "trader4",
-            trader4,
-            await methods.getTAContractAddress(),
-            trader4,
-            gasLimit);
+        // await methods.createTrader(
+        //     trader4,
+        //     "trader4",
+        //     trader4,
+        //     await methods.getTAContractAddress(),
+        //     trader4,
+        //     gasLimit);
 
-        //Agreement 1
-        await methods.createAgreement(
-            agreement1Id,
-            trader1,
-            trader2,
-            30,
-            trackId,
-            await methods.getTraderContractAddress(), //TODO: fix this
-            await methods.getTrackContractAddress(),
-            trader1,
-            gasLimit
-        );
+        // //Agreement 1
+        // await methods.createAgreement(
+        //     agreement1Id,
+        //     trader1,
+        //     trader2,
+        //     30,
+        //     trackId,
+        //     await methods.getTraderContractAddress(), //TODO: fix this
+        //     await methods.getTrackContractAddress(),
+        //     trader1,
+        //     gasLimit
+        // );
 
-        //Agreement 2
-        await methods.createAgreement(
-            agreement2Id,
-            trader1,
-            trader3,
-            50,
-            trackId,
-            await methods.getTraderContractAddress(),
-            await methods.getTrackContractAddress(),
-            trader1,
-            gasLimit
-        );
+        // //Agreement 2
+        // await methods.createAgreement(
+        //     agreement2Id,
+        //     trader1,
+        //     trader3,
+        //     50,
+        //     trackId,
+        //     await methods.getTraderContractAddress(),
+        //     await methods.getTrackContractAddress(),
+        //     trader1,
+        //     gasLimit
+        // );
 
-        //Agreement 3
-        await methods.createAgreement(
-            agreement3Id,
-            trader3,
-            trader4,
-            90,
-            trackId,
-            await methods.getTraderContractAddress(),
-            await methods.getTrackContractAddress(),
-            trader1,
-            gasLimit
-        );
+        // //Agreement 3
+        // await methods.createAgreement(
+        //     agreement3Id,
+        //     trader3,
+        //     trader4,
+        //     90,
+        //     trackId,
+        //     await methods.getTraderContractAddress(),
+        //     await methods.getTrackContractAddress(),
+        //     trader1,
+        //     gasLimit
+        // );
 
-        await methods.distribution(
-            trackId,
-            trader1,
-            new Date().getTime(),
-            trader1,
-            gasLimit);
+        // await methods.distribution(
+        //     trackId,
+        //     trader1,
+        //     new Date().getTime(),
+        //     trader1,
+        //     gasLimit);
 
-        //Results after
-        console.log('TRADERS OUTPUT----------------------');
-            console.log(trader1);
-        //Trader1
-        let trader1Result = await methods.getTrader(
-            trader1,
-            trader1,
-            gasLimit);
+        // //Results after
+        // console.log('TRADERS OUTPUT----------------------');
+        //     console.log(trader1);
+        // //Trader1
+        // let trader1Result = await methods.getTrader(
+        //     trader1,
+        //     trader1,
+        //     gasLimit);
 
-        let tokenTrader1Result = await methods.getTokenAccount(
-            trader1,
-            trader1,
-            gasLimit
-        );
+        // let tokenTrader1Result = await methods.getTokenAccount(
+        //     trader1,
+        //     trader1,
+        //     gasLimit
+        // );
 
-        console.log('TRADER ' + trader1Result.name + ' *******');
-        console.log('BALANCE DISABLED:');
-        console.log(tokenTrader1Result.balanceDisabled);
-        console.log('BALANCE ENABLED');
-        console.log(tokenTrader1Result.balanceEnabled);
+        // console.log('TRADER ' + trader1Result.name + ' *******');
+        // console.log('BALANCE DISABLED:');
+        // console.log(tokenTrader1Result.balanceDisabled);
+        // console.log('BALANCE ENABLED');
+        // console.log(tokenTrader1Result.balanceEnabled);
 
-        //Trader2
-        let trader2Result = await methods.getTrader(
-            trader2,
-            trader2,
-            gasLimit);
+        // //Trader2
+        // let trader2Result = await methods.getTrader(
+        //     trader2,
+        //     trader2,
+        //     gasLimit);
 
-        let tokenTrader2Result = await methods.getTrader(
-            trader2,
-            trader2,
-            gasLimit
-        );
+        // let tokenTrader2Result = await methods.getTrader(
+        //     trader2,
+        //     trader2,
+        //     gasLimit
+        // );
 
-        console.log('TRADER ' + trader2Result.name + ' *******');
-        console.log('BALANCE DISABLED:');
-        console.log(tokenTrader2Result.balanceDisabled);
-        console.log('BALANCE ENABLED');
-        console.log(tokenTrader2Result.balanceEnabled);
+        // console.log('TRADER ' + trader2Result.name + ' *******');
+        // console.log('BALANCE DISABLED:');
+        // console.log(tokenTrader2Result.balanceDisabled);
+        // console.log('BALANCE ENABLED');
+        // console.log(tokenTrader2Result.balanceEnabled);
 
-        //Trader 3
-        let trader3Result = await methods.getTrader(
-            trader3,
-            trader3,
-            gasLimit);
+        // //Trader 3
+        // let trader3Result = await methods.getTrader(
+        //     trader3,
+        //     trader3,
+        //     gasLimit);
 
-        let tokenTrader3Result = await methods.getTrader(
-            trader3,
-            trader3,
-            gasLimit
-        );
-        console.log('TRADER ' + trader3Result.name + ' *******');
-        console.log('BALANCE DISABLED:');
-        console.log(tokenTrader3Result.balanceDisabled);
-        console.log('BALANCE ENABLED');
-        console.log(tokenTrader3Result.balanceEnabled);
+        // let tokenTrader3Result = await methods.getTrader(
+        //     trader3,
+        //     trader3,
+        //     gasLimit
+        // );
+        // console.log('TRADER ' + trader3Result.name + ' *******');
+        // console.log('BALANCE DISABLED:');
+        // console.log(tokenTrader3Result.balanceDisabled);
+        // console.log('BALANCE ENABLED');
+        // console.log(tokenTrader3Result.balanceEnabled);
 
-        //Trader4
-        let trader4Result = await methods.getTrader(
-            trader4,
-            trader4,
-            gasLimit);
+        // //Trader4
+        // let trader4Result = await methods.getTrader(
+        //     trader4,
+        //     trader4,
+        //     gasLimit);
 
-        let tokenTrader4Result = await methods.getTrader(
-            trader4,
-            trader4,
-            gasLimit
-        );
+        // let tokenTrader4Result = await methods.getTrader(
+        //     trader4,
+        //     trader4,
+        //     gasLimit
+        // );
 
-        console.log('TRADER ' + trader4Result.name + ' *******');
-        console.log('BALANCE DISABLED:');
-        console.log(tokenTrader4Result.balanceDisabled);
-        console.log('BALANCE ENABLED');
-        console.log(tokenTrader4Result.balanceEnabled);
+        // console.log('TRADER ' + trader4Result.name + ' *******');
+        // console.log('BALANCE DISABLED:');
+        // console.log(tokenTrader4Result.balanceDisabled);
+        // console.log('BALANCE ENABLED');
+        // console.log(tokenTrader4Result.balanceEnabled);
     } catch (error) {
         console.log(error)
     }
