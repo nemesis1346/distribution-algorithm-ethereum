09de73228f6539a3451497e83dd3f9ea562bf60b
tony <nemesis1346@uofs-10-230-64-199.usask.ca>
2019-05-09 16:05:59 -0600

    trying ropsten connection

--- ethereum-node-server/truffle.js
+++ ethereum-node-server/truffle.js
@@ -1,5 +1,7 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+require('babel-register');
+require('dotenv').config();
+const HDWalletProvider=require('truffle-hdwallet-provider');
 
 module.exports = {
   networks: {
@@ -7,6 +9,22 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*'
+    },
+    ropsten:{
+      provider:function(){
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://ropsten.infura.io/${process.env.INFURA_API_KEY}"
+          );
+      },
+      gasPrice:25000000000,
+      network_id:3
+    }
+  },
+  solc:{
+    optimizer:{
+      enabled:true,
+      runs:200
     }
   }
 }
