9b187f1c54a258ccc01c449b5407f82c4eb00ade
marco <marco.maigua1346@gmail.com>
2019-11-02 11:08:11 -0500

    git development, object error must be fixed

---
+++ react-app/src/api/middlewareHandler.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { store } from '../reactapp';
+import { parseResponse } from '../utils/Utils'; //must be changed
+import {saveTokenObject, logout} from '../actions/authActions'; 
+import * as CONFIG from '../constants/config';
+
+//INSTANCE OF AXIOS FOR LARAVEL
+
+export const instanceWithInterceptors = axios.create({ baseURL: process.env.MIX_BASE_URL });
+
+instanceWithInterceptors.defaults.headers.common.Accept = 'application/json';
+instanceWithInterceptors.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+instanceWithInterceptors.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+instanceWithInterceptors.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+instanceWithInterceptors.defaults.params = {};
+
+//LISTEN FOR THE TOKEN HERE and ERRORS if you like
+instanceWithInterceptors.interceptors.response.use(
+    (response) => {
+        console.log('RESPONSE IN INTERCEPTORS');
+        console.log(response);
+        let result = parseResponse(response);
+        console.log(result);
+
+        if (result.tokenObject != null && result.tokenObject != "") {
+            console.log('NEW TOKENS IN INTERCEPTORS');
+            console.log(result.tokenObject);
+            store.dispatch(saveTokenObject(result.tokenObject));
+        }
+        return result;
+    },
+    (error) => {
+        let state = store.getState();
+
+        console.log('ERRORS IN RESPONSE USING INTERCEPTORS IN LOGIN');
+       // store.dispatch(logout(state.userReducer.userSession));
+
+        return Promise.reject(error);
+    });
+
+
+//ADD TOKENS FOR ALL REQUESTS here
+instanceWithInterceptors.interceptors.request.use(function (config) {
+    console.log('USING REQUEST INTERCEPTOR');
+    let state = store.getState();
+    console.log('TOKEN OBJECT');
+    console.log(state.userReducer.tokenObject);
+    
+    if (state.userReducer.tokenObject != null&&
+        state.userReducer.tokenObject != "") {
+        console.log('Need authorization');
+        let access_token = state.userReducer.tokenObject.access_token;
+        let refresh_token = state.userReducer.tokenObject.refresh_token;
+        config.headers.Authorization = 'Bearer ' + access_token;
+        config.params = {
+            refresh_token: refresh_token,
+            client_id: CONFIG.CLIENT_ID,
+            client_secret: CONFIG.CLIENT_SECRET,
+        }
+        console.log(JSON.stringify(config));
+    }
+    return config;
+}, function (err) {
+    return Promise.reject(err);
+});
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
+import LocalStorage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
+//Store Configuration
+const persistConfig = {
+    key: 'root',
+    storage: LocalStorage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     applyMiddleware(thunk)
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+export const persistor = persistStore(store);
+
+
+ReactDOM.render(<Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+        <App />
+    </PersistGate>
+</Provider>, document.getElementById('root'));
